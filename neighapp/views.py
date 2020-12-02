9da3c0b5cd03e0d5e9ed2117307b992a035b2e3a
RonaldKiprotich <protich12@gmail.com>
2020-12-02 11:01:49 +0300

    admin profilelist views

--- neighapp/views.py
+++ neighapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import *
 
 # Create your views here.
+
+class AdminProfileList(APIView):
+    def get(self, request, format=None):
+        all_merch = AdminProfile.objects.all()
+        serializers = ProfileSerializer(all_merch, many=True)
+        return Response(serializers.data)
+
+
