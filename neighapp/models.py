e0bae437aef805e95fdca47397bf1793d8a27ee9
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-12-02 11:06:15 +0300

    added more fields to models

--- neighapp/models.py
+++ neighapp/models.py
@@ -3,14 +3,28 @@ from django.db import models
 
 # Create your models here.
 class Occupant(models.Model):
-    user = models.OneToOneField(User)
-    name = models.CharField()
-    profile_pic = CloudinaryField()
-    neighbourhood = models.ForeignKey(Neighbourhood)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+    profile_pic = CloudinaryField('image')
+    neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE, related_name='occupant')
+
 class Business(models.Model):
-    name = models.CharField()
-    definition = models.CharField()
-    email = models.EmailField()
-    neighbourhood = models.ForeignKey(Neighbourhood)
-    location = LocationField()
+    name = models.CharField(max_length=120)
+    email = models.EmailField(max_length=254)
+    description = models.TextField(blank=True)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE, related_name='business')
+    location = models.CharField(max_length=60)
+
+    def __str__(self):
+        return f'{self.name} Business'
+
+    def create_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    @classmethod
+    def search_business(cls, name):
+        return cls.objects.filter(name__icontains=name).all()
 
