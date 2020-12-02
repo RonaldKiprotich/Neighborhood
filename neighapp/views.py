2f211f81ac5a90552e4225e58b6a21ac21f8049b
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-12-02 11:25:26 +0300

    created registration form in the forms file

--- neighapp/views.py
+++ neighapp/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import RegistrationForm
 from django.contrib import messages
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request,'Your account has been created successfully')
             return redirect('home_view')
         else:
-            form = UserCreationForm()
+            form = RegistrationForm()
     context = {
         'form':form,
     }
