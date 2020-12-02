1975f326d3946849466b37c59aac099198e7419c
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-12-02 10:09:00 +0300

    created a register view function to handle registration of users

--- neighapp/models.py
+++ neighapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Business(models.Model):
+    name = models.CharField()
+    definition = models.CharField()
+    email = models.EmailField()
+    neighbourhood = models.ForeignKey(Neighbourhood)
+    location = LocationField()
+
+
+class Occupant(models.Model):
+    user = models.OneToOneField(User)
+    name = models.CharField()
+    profile_pic = CloudinaryField()
+    neighbourhood = models.ForeignKey(Neighbourhood)
