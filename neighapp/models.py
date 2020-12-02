e77fbde9240c8691ba71a8d826adf5361afcd414
RonaldKiprotich <protich12@gmail.com>
2020-12-02 09:48:23 +0300

    add neighborhood class

--- neighapp/models.py
+++ neighapp/models.py
@@ -16,3 +16,30 @@ class AdminProfile(models.Model):
 
     class Meta:
         db_table = 'adminprof'
+
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    admin = models.ForeignKey("AdminProfile", on_delete=models.CASCADE, related_name='neighborhood')
+    hoodLogo = CloudinaryField('image')
+    definition = models.TextField()
+    occupants = models.IntegerField(null=True, blank=True)
+    emergencyNo = models.IntegerField(null=True, blank=True)
+
+
+    class Meta:
+        db_table = 'neighborhood'
+    
+
+    def __str__(self):
+        return f'{self.name} neighborhood'
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def find_neighborhood(cls, neighborhood_id):
+        return cls.objects.filter(id=neighborhood_id)
