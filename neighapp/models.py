495907aedd0cb8d82f1c303f6c15f23b0250c4a5
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-12-02 10:44:51 +0300

    added occcupants and business models

--- neighapp/models.py
+++ neighapp/models.py
@@ -2,4 +2,15 @@ from django.db import models
 
 
 # Create your models here.
+class Occupant(models.Model):
+    user = models.OneToOneField(User)
+    name = models.CharField()
+    profile_pic = CloudinaryField()
+    neighbourhood = models.ForeignKey(Neighbourhood)
+class Business(models.Model):
+    name = models.CharField()
+    definition = models.CharField()
+    email = models.EmailField()
+    neighbourhood = models.ForeignKey(Neighbourhood)
+    location = LocationField()
 
