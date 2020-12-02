dc1679304ecb1d5f96892f58ca48c2407a54e600
RonaldKiprotich <protich12@gmail.com>
2020-12-02 08:29:07 +0300

    add adminprofile class

--- neighapp/models.py
+++ neighapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class AdminProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = CloudinaryField('image')
+    bio = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    class Meta:
+        db_table = 'profile'
