bf22702a83116a4724e53c9090c9067cedc98790
RonaldKiprotich <protich12@gmail.com>
2020-12-02 10:27:37 +0300

    add neighserializer class

--- neighapp/serializer.py
+++ neighapp/serializer.py
@@ -5,3 +5,8 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = AdminProfile
         fields = ('user', 'profile_pic', 'bio')
+
+class NeighSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NeighbourHood
+        fields = ('name', 'location', 'admin','hoodLogo','definition','occupants','emergencyNo')
