d64782ee7534fb892bb02454a2dc10e5bef5e475
RonaldKiprotich <protich12@gmail.com>
2020-12-02 07:37:47 +0300

    configure urls

---
+++ neighapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView 
+
+
+urlpatterns = [
+    
+]
