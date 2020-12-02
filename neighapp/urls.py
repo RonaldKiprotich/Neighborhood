ad32cbb2747b8b519d57cc4c9cfa76b2b3fe8f45
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-12-02 12:35:16 +0300

    fixing errors

--- neighapp/urls.py
+++ neighapp/urls.py
@@ -8,5 +8,6 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('register/',views.register,name='register'),
+    path('login/',LoginView.as_view(), name='login'),
     
 ]
