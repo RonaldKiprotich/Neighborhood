cd75fd47774bbf3568b71f32474e2a19c9561fe3
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-12-02 10:25:39 +0300

    added validation functionality to validate userdetails during registration

--- neighapp/views.py
+++ neighapp/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Your account has been created successfully')
+            return redirect('home_view')
+        else:
+            form = UserCreationForm()
     context = {
         'form':form,
     }
